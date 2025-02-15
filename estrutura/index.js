8a16b449ec25b44e0168cbb02b81f971ec74de20
mateusborges20 <borgesmateus099@gmail.com>
2025-02-15 19:02:51 -0300

    adicionando scroll suave, e modificando links

---
+++ estrutura/index.js
@@ -0,0 +1,28 @@
+
+//scroll suave
+
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"');
+
+console.log(linksInternos);
+
+
+function scrollToSection(event) {
+  event.preventDefault();
+
+  const href = event.currentTarget.getAttribute('href');
+
+  const section = document.querySelector(href);
+  console.log(section);
+
+  section.scrollIntoView( {
+    behavior: 'smooth',
+    block: 'start',
+  })
+  
+  
+}
+
+linksInternos.forEach((link) => {
+  link.addEventListener('click', scrollToSection);
+});
+
--- script.js
+++ script.js
@@ -18,3 +18,27 @@ function initAcordion() {
 }
 
 initAcordion();
+
+//scroll suave
+
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"');
+
+function scrollToSection(event) {
+  event.preventDefault();
+
+  const href = event.currentTarget.getAttribute('href');
+
+  const section = document.querySelector(href);
+  console.log(section);
+
+  section.scrollIntoView( {
+    behavior: 'smooth',
+    block: 'start',
+  })
+  
+  
+}
+
+linksInternos.forEach((link) => {
+  link.addEventListener('click', scrollToSection);
+});
